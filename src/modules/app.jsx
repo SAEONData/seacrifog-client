574a4fe0043b6652088ee22e8ef0936ae7ecee5f
Zach Smith <zachsmith.sa@gmail.com>
2019-08-07 16:44:38 +0200

    added (what I think to be) a sensible layout for specifying navigation items, and page components. due to the way that react-router works it is sensible to specify these two lists separately

--- src/modules/app.jsx
+++ src/modules/app.jsx
@@ -3,19 +3,39 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Navigation from './layout'
+import Home from './home'
 
 // Configure the Apollo Client
 const cache = new InMemoryCache()
 const link = new HttpLink({ uri: 'http://localhost:3000/graphql' })
 const client = new ApolloClient({ cache, link })
 
+// Specify the items in the side menu
+const navItems = [
+  {
+    exact: true,
+    label: 'home',
+    to: '/',
+    icon: 'home',
+    nestedItems: false
+  }
+]
+
 // SPA wrapper
 const App = () => (
   <BrowserRouter>
     <ApolloProvider client={client}>
-      <Navigation />
+      <Navigation navItems={navItems}>
+        <Route key={'route-to-home'} path={'/'} exact={true} component={Home} />
+        <Route
+          key={'route-to-home'}
+          path={'/home'}
+          exact={true}
+          component={Home}
+        />
+      </Navigation>
     </ApolloProvider>
   </BrowserRouter>
 )
--- src/modules/layout/index.jsx
+++ src/modules/layout/index.jsx
@@ -1,22 +1,31 @@
 import React, { Component } from 'react'
 import { NavigationDrawer } from 'react-md'
 import { withRouter } from 'react-router'
-import { Switch, Route } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
 import NavItemLink from './nav-item-link'
 
-const navItems = [
-  {
-    exact: true,
-    label: 'home',
-    to: '/',
-    icon: 'home'
-  }
-]
+const buildNavItemLinks = (navItems, parent = false) =>
+  navItems.map(route => (
+    <NavItemLink
+      key={'route-' + route.label}
+      nestedItems={
+        route.nestedItems && route.nestedItems.length > 0
+          ? buildNavItemLinks(route.nestedItems, true)
+          : null
+      }
+      hasParent={parent}
+      label={route.label}
+      to={route.to}
+      icon={route.icon}
+      exact
+    />
+  ))
 
 class Navigation extends Component {
   constructor(props) {
     super(props)
     this.state = { toolbarTitle: this.getCurrentTitle(props) }
+    this.navItems = props.navItems
   }
 
   componentWillReceiveProps(nextProps) {
@@ -29,10 +38,16 @@ class Navigation extends Component {
 
   render() {
     const { toolbarTitle } = this.state
-    const { location } = this.props
+    const { location, navItems } = this.props
     return (
-      <NavigationDrawer drawerTitle="SEACRIFOG" navItems={navItems}>
-        <Switch key={location.pathname || '/home'}></Switch>
+      <NavigationDrawer
+        drawerTitle="SEACRIFOG"
+        navItems={buildNavItemLinks(navItems)}
+        toolbarTitle={toolbarTitle || ' '}
+      >
+        <Switch key={location.pathname || '/home'}>
+          {this.props.children}
+        </Switch>
       </NavigationDrawer>
     )
   }
