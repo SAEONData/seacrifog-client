1d3427c13b287d6be8cfe86edc9cb9051e211f2a
Zach Smith <zachsmith.sa@gmail.com>
2019-08-21 17:03:25 +0200

    adjusted gql queries to fetch all variable and protocol fields. extended the graphql test to use the result of the graphql data

--- src/pages/gql-example/index.jsx
+++ src/pages/gql-example/index.jsx
@@ -1,18 +1,64 @@
 import React from 'react'
 import QueryRenderer from '../../modules/query-renderer'
-import { Card, CardTitle, CardText, Grid, Cell } from 'react-md'
 import { VARIABLES } from '../../graphql/queries'
+import {
+  Grid,
+  Cell,
+  DropdownMenu,
+  ListItem,
+  FontIcon,
+  TextField,
+  Card,
+  CardTitle,
+  CardText
+} from 'react-md'
+
+// eslint-disable-next-line no-extend-native
+String.prototype.truncate = function(length, ending) {
+  length = length || 100
+  ending = ending || '...'
+  if (this.length > length)
+    return this.substring(0, length - ending.length) + ending
+  else return this
+}
 
 export default () => (
   <QueryRenderer query={VARIABLES}>
     {data => (
       <Grid>
-        <Cell size={12}>
+        <Cell size={6} tabletSize={8} phoneSize={6}>
           <Card>
-            <CardTitle title="This is a title" subtitle="And subtitle" />
+            <CardTitle
+              title="GQL Usage examples"
+              subtitle="Testing using API data with GQL components"
+            />
             <CardText>
-              <p>{JSON.stringify(data)}</p>
-              <p>Edit me in /src/modules/gql-example/index.jsx</p>
+              <DropdownMenu
+                style={{ width: '100%' }}
+                id="variable-search"
+                menuItems={data.variables.map(v => (
+                  <ListItem
+                    leftAvatar={<FontIcon>chevron_right</FontIcon>}
+                    primaryText={v.name}
+                    secondaryText={v.description}
+                    threeLines
+                  />
+                ))}
+                anchor={{
+                  x: DropdownMenu.HorizontalAnchors.INNER_LEFT,
+                  y: DropdownMenu.VerticalAnchors.BOTTOM
+                }}
+                position={DropdownMenu.Positions.BELOW}
+              >
+                <TextField
+                  id="search-variables-text"
+                  label="Search"
+                  autoComplete="off"
+                  value={''}
+                  placeholder="Search variables by anything"
+                  onChange={val => console.log('hi')}
+                />
+              </DropdownMenu>
             </CardText>
           </Card>
         </Cell>
