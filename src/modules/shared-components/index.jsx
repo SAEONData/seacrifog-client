c04a6b8286382794dc08db52c67677b2fb12c2e8
Zach Smith <zachsmith.sa@gmail.com>
2019-09-09 14:38:20 +0200

    removed unused component

--- src/modules/shared-components/index.jsx
+++ src/modules/shared-components/index.jsx
@@ -1,18 +1,6 @@
 import React from 'react'
 import { Button, Grid, Cell } from 'react-md'
 
-export const GoToButton = ({ id }) => (
-  <Button
-    secondary
-    tooltipLabel={`Go to ${id}`}
-    tooltipPosition="top"
-    onClick={() => alert('this will navigate to the variable that is clicked')}
-    icon
-  >
-    remove_red_eye
-  </Button>
-)
-
 export const NoneMessage = () => <p style={{ textAlign: 'center' }}>(None)</p>
 
 export const FormattedInfo = ({ object }) => (
