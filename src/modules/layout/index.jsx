d698b611cc0bde0abc8f77b7015fb796e27fe0d7
Zach Smith <zachsmith.sa@gmail.com>
2019-09-12 09:09:03 +0200

    added a bottom border for the toolbar

--- src/modules/layout/index.jsx
+++ src/modules/layout/index.jsx
@@ -34,7 +34,7 @@ class Navigation extends Component {
           divider || subheader ? { divider, subheader, ...navItem } : <NavItemLink key={'route-' + navItem.keyval} {...navItem} />
         )}
         toolbarZDepth={0}
-        toolbarStyle={{ backgroundColor: '#fff' }}
+        toolbarStyle={{ backgroundColor: '#fff', borderBottom: '1px solid rgba(0,0,0,0.1)' }}
         toolbarThemeType="themed"
         toolbarClassName="sf-toolbar"
         drawerZDepth={0}
