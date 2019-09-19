59dd86b06ffb886452f1cca7750f19f603c2716e
Zach Smith <zachsmith.sa@gmail.com>
2019-09-19 12:37:46 +0200

    improved map layout of dataproducts

--- src/pages/explorer-dataproducts/index.jsx
+++ src/pages/explorer-dataproducts/index.jsx
@@ -20,89 +20,100 @@ export default ({ updateForm, hoveredDP, selectedDP, ...props }) => (
           t3={selectedDP ? selectedDP.contact : hoveredDP ? hoveredDP.contact : ''}
         />
 
-        {/* Main Table (selectable) */}
         <Grid>
           <Cell size={12}>
-            <Card tableCard>
-              {' '}
-              <Table
-                invisibleHeaders={['EDIT']}
-                headers={['EDIT'].concat(Object.keys(dataproducts[0] || '').filter(col => col !== '__typename' && col !== 'id'))}
-                data={dataproducts.map(d => mergeLeft({ EDIT: <EditButton to={`/dataproducts/${d.id}`} /> }, d))}
-                initialSearch={
-                  props.history.location.search
-                    ? q.parse(props.history.location.search, { ignoreQueryPrefix: true }).searchTerm
-                    : selectedDP
-                    ? selectedDP.title
-                    : ''
-                }
-                onRowClick={row => updateForm({ selectedDP: row })}
-                onRowHover={row => updateForm({ hoveredDP: row })}
-                selectedRow={selectedDP}
-                toolbarButtons={[
-                  <LinkButton key={'url-button'} active={selectedDP ? false : true} />,
-                  <DownloadButton key={'download-button'} active={selectedDP ? false : true} />
-                ]}
-                resetForm={() => updateForm({ selectedDP: null })}
-              />
-            </Card>
-            {/* Display information about selected row */}
-            {selectedDP ? (
-              <DataQuery query={DATAPRODUCT} variables={{ id: selectedDP.id }}>
-                {({ dataproduct }) => (
-                  <>
-                    <ExpansionList>
-                      <ExpansionPanel label="Abstract" defaultExpanded footer={false}>
-                        <Grid>
-                          <Cell size={12}>
-                            <p>{dataproduct.abstract}</p>
-                          </Cell>
-                        </Grid>
-                      </ExpansionPanel>
-                      <ExpansionPanel label="Additional Information" footer={false}>
-                        {
-                          <FormattedInfo
-                            object={pickBy((val, key) => {
-                              if (['abstract', '__typename'].includes(key)) return false
-                              if (typeof val === 'object') return false
-                              return true
-                            }, dataproduct)}
-                          />
-                        }
-                      </ExpansionPanel>
-                      <ExpansionPanel contentStyle={{ paddingTop: 16 }} label="Spatial Coverage Information" footer={false}>
-                        <CoverageMap geoJson={dataproduct.coverage_spatial} />
-                      </ExpansionPanel>
-                    </ExpansionList>
+            {/* Main Table (selectable) */}
+            <Grid noSpacing>
+              <Cell size={12}>
+                <Card tableCard>
+                  <Table
+                    invisibleHeaders={['EDIT']}
+                    headers={['EDIT'].concat(Object.keys(dataproducts[0] || '').filter(col => col !== '__typename' && col !== 'id'))}
+                    data={dataproducts.map(d => mergeLeft({ EDIT: <EditButton to={`/dataproducts/${d.id}`} /> }, d))}
+                    initialSearch={
+                      props.history.location.search
+                        ? q.parse(props.history.location.search, { ignoreQueryPrefix: true }).searchTerm
+                        : selectedDP
+                        ? selectedDP.title
+                        : ''
+                    }
+                    onRowClick={row => updateForm({ selectedDP: row })}
+                    onRowHover={row => updateForm({ hoveredDP: row })}
+                    selectedRow={selectedDP}
+                    toolbarButtons={[
+                      <LinkButton key={'url-button'} active={selectedDP ? false : true} />,
+                      <DownloadButton key={'download-button'} active={selectedDP ? false : true} />
+                    ]}
+                    resetForm={() => updateForm({ selectedDP: null })}
+                  />
+                </Card>
+              </Cell>
 
-                    <h3 style={{ textAlign: 'center', marginTop: '100px', marginBottom: '50px' }}>Essential Variables</h3>
-                    {dataproduct.variables[0] ? (
-                      <Card tableCard>
-                        <Table
-                          onRowClick={row => updateForm({ selectedVariable: row }, () => props.history.push(`/variables?searchTerm=${row.name}`))}
-                          headers={Object.keys(dataproduct.variables[0])
-                            .filter(col => col !== '__typename' && col !== 'id')
-                            .concat('Relationship')}
-                          data={dataproduct.variables.map(v => mergeLeft({ relationship: 'direct' }, v))}
-                          tableStyle={{}}
-                          toolbarButtons={[]}
-                        />
-                      </Card>
-                    ) : (
-                      <NoneMessage />
+              <Cell size={12}>
+                {/* Display information about selected row */}
+                {selectedDP ? (
+                  <DataQuery query={DATAPRODUCT} variables={{ id: selectedDP.id }}>
+                    {({ dataproduct }) => (
+                      <Grid noSpacing>
+                        <Cell size={12}>
+                          <ExpansionList>
+                            <ExpansionPanel label="Abstract" defaultExpanded footer={false}>
+                              <Grid>
+                                <Cell size={12}>
+                                  <p>{dataproduct.abstract}</p>
+                                </Cell>
+                              </Grid>
+                            </ExpansionPanel>
+                            <ExpansionPanel label="Additional Information" footer={false}>
+                              {
+                                <FormattedInfo
+                                  object={pickBy((val, key) => {
+                                    if (['abstract', '__typename'].includes(key)) return false
+                                    if (typeof val === 'object') return false
+                                    return true
+                                  }, dataproduct)}
+                                />
+                              }
+                            </ExpansionPanel>
+                            <ExpansionPanel className="fix-panel-content-style" label="Spatial Coverage" footer={false}>
+                              <CoverageMap geoJson={dataproduct.coverage_spatial} />
+                            </ExpansionPanel>
+                          </ExpansionList>
+                        </Cell>
+                        <Cell size={12}>
+                          <h3 style={{ textAlign: 'center', marginTop: '100px', marginBottom: '50px' }}>Essential Variables</h3>
+                          {dataproduct.variables[0] ? (
+                            <Card tableCard>
+                              <Table
+                                onRowClick={row =>
+                                  updateForm({ selectedVariable: row }, () => props.history.push(`/variables?searchTerm=${row.name}`))
+                                }
+                                headers={Object.keys(dataproduct.variables[0])
+                                  .filter(col => col !== '__typename' && col !== 'id')
+                                  .concat('relationship')}
+                                data={dataproduct.variables.map(v => mergeLeft({ relationship: 'direct' }, v))}
+                                tableStyle={{}}
+                                toolbarButtons={[]}
+                              />
+                            </Card>
+                          ) : (
+                            <NoneMessage />
+                          )}
+                        </Cell>
+                      </Grid>
                     )}
-                  </>
+                  </DataQuery>
+                ) : (
+                  <Grid>
+                    <Cell size={12}>
+                      <p>
+                        <i>Select a row for more detailed information</i>
+                      </p>
+                    </Cell>
+                  </Grid>
                 )}
-              </DataQuery>
-            ) : (
-              <Grid>
-                <Cell size={12}>
-                  <p>
-                    <i>Select a row for more detailed information</i>
-                  </p>
-                </Cell>
-              </Grid>
-            )}
+              </Cell>
+            </Grid>
           </Cell>
         </Grid>
       </>
