82573a496b6fece6e5d42cc8996cc8d724f85b94
Zach Smith <zachsmith.sa@gmail.com>
2019-09-23 15:54:18 +0200

    added SVG icon test to /test

--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
 import DataQuery from '../../modules/data-query'
 import { VARIABLE } from '../../graphql/queries'
-import { Button } from 'react-md'
+import { Button, Grid, Cell, SVGIcon, Card, CardText } from 'react-md'
+import { DatabaseIcon } from '../../icons'
+
+const InlineSvgIcon = ({ size }) => (
+  <SVGIcon primary size={size} desc={'Test SVG icon'}>
+    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+  </SVGIcon>
+)
+
+const ZachsTests = () => (
+  <Grid>
+    <Cell size={12}>
+      <Card>
+        <CardText>
+          <Button icon>
+            <DatabaseIcon primary={true} />
+          </Button>
+        </CardText>
+        <CardText>
+          <InlineSvgIcon size={48} />
+        </CardText>
+        <CardText>
+          <Button icon>
+            <InlineSvgIcon />
+          </Button>
+        </CardText>
+      </Card>
+    </Cell>
+  </Grid>
+)
 
 export default () => (
   <DataQuery query={VARIABLE} variables={{ id: 4 }}>
@@ -19,6 +48,7 @@ export default () => (
         >
           Get my domain
         </Button>
+        <ZachsTests />
       </>
     )}
   </DataQuery>
