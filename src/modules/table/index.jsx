0dc78879a9fc9836eb8212571a504eb806504b84
Zach Smith <zachsmith.sa@gmail.com>
2019-09-23 11:33:24 +0200

    Updated table to include sorting. Also had to redo some of the logic WRT to HOW sorting is applied for performance reasons

--- src/modules/table/index.jsx
+++ src/modules/table/index.jsx
@@ -5,26 +5,37 @@ import { log } from '../../lib/log'
 import { Toolbar, TextField, FontIcon, DataTable, TableHeader, TableRow, TableColumn, TableBody, TablePagination, Button } from 'react-md'
 
 export default class extends PureComponent {
-  state = {}
   headers = {}
 
   constructor(props) {
     super(props)
     this.rows = this.props.data.length
-    this.state.slice = [0, 5]
-    this.state.searchValue = this.props.initialSearch ? this.props.initialSearch : ''
-
-    // Some headers shouldn't get column names
     this.invisibleHeaders = this.props.invisibleHeaders || []
 
-    // Some columns should ignore row clicks
-    this.unlickableCols = this.props.unlickableCols || []
+    // Setup state
+    const state = {
+      searchValue: this.props.initialSearch ? this.props.initialSearch : '',
+      slice: [0, 5],
+      sorting: {}
+    }
 
     // Setup headers for keeping header-column values aligned
-    this.props.headers.forEach((h, i) => (this.headers[h] = i))
+    // Also, setup sorting
+    this.props.headers.forEach((h, i) => {
+      this.headers[h] = i
+      state.sorting[h] = {
+        sorted: false,
+        ascending: true
+      }
+    })
+
+    state.filteredData = this.getFilteredData(state.searchValue)
+
+    this.state = state
   }
 
   getFilteredData = searchValue => {
+    // First get filtered data
     const filteredData = this.props.data.filter(p => {
       const term = searchValue.toUpperCase().trim()
       if (term === '') return true
@@ -41,7 +52,31 @@ export default class extends PureComponent {
       }
       return false
     })
-    return filteredData
+
+    // Then apply sorting
+    const { sorting } = this.state || {}
+    let sortedCol
+    // TODO: This is currently a bug in react-scripts 3.1.1
+    // eslint-disable-next-line
+    for (let col in sorting) {
+      if (sorting[col].sorted) sortedCol = col
+    }
+
+    const asending = sortedCol ? (sorting[sortedCol].ascending ? -1 : 1) : 0
+
+    // Then return
+    return sortedCol
+      ? filteredData.sort((a, b) => {
+          const aSortVal = typeof a[sortedCol] === 'string' ? a[sortedCol].toUpperCase() : parseFloat(a[sortedCol] || 0)
+          const bSortVal = typeof b[sortedCol] === 'string' ? b[sortedCol].toUpperCase() : parseFloat(b[sortedCol] || 0)
+          return aSortVal > bSortVal ? asending : aSortVal < bSortVal ? -asending : 1
+        })
+      : filteredData
+  }
+
+  setFilteredData = () => {
+    const filteredData = this.getFilteredData(this.state.searchValue)
+    this.setState({ filteredData })
   }
 
   getDataSlice = data => {
@@ -53,10 +88,22 @@ export default class extends PureComponent {
     this.setState({ slice: [start, start + rowsPerPage] })
   }
 
+  applySorting = colName => {
+    const sorting = Object.assign({}, this.state.sorting)
+    Object.keys(sorting).forEach(key => {
+      if (key !== colName) sorting[key].sorted = false
+      else {
+        sorting[key].ascending = !sorting[key].ascending
+        sorting[key].sorted = true
+      }
+    })
+    this.setState({ sorting }, () => this.setFilteredData())
+  }
+
   render() {
-    const { searchValue } = this.state
+    const { searchValue, sorting, filteredData } = this.state
     const { selectedRow, toolbarButtons, toolbarStyle } = this.props
-    const { headers, invisibleHeaders: specialHeaders } = this
+    const { headers, invisibleHeaders: specialHeaders, applySorting } = this
     const resetForm = this.props.resetForm || null
     const onRowHover = this.props.onRowHover || (() => log('Row hover changed'))
     const onRowClick = this.props.onRowClick || (() => log('Row selection changed'))
@@ -70,7 +117,7 @@ export default class extends PureComponent {
             block={true}
             autoComplete={'off'}
             value={searchValue}
-            onChange={val => this.setState({ searchValue: val })}
+            onChange={val => this.setState({ searchValue: val }, () => this.setFilteredData())}
             placeholder="Search by table fields..."
             leftIcon={<FontIcon>search</FontIcon>}
           />
@@ -84,7 +131,7 @@ export default class extends PureComponent {
               style={{ display: 'flex', marginRight: '20px' }}
               icon
               onClick={() => {
-                this.setState({ searchValue: '' })
+                this.setState({ searchValue: '' }, () => this.setFilteredData())
                 if (resetForm) resetForm()
               }}
             >
@@ -96,24 +143,22 @@ export default class extends PureComponent {
           <TableHeader>
             <TableRow>
               {Object.keys(headers).map((header, i) => (
-                <TableColumn role="button" key={i} style={{ textAlign: 'center' }}>
+                <TableColumn
+                  sorted={specialHeaders.includes(header) ? null : sorting[header].ascending}
+                  sortIcon={<FontIcon>keyboard_arrow_up</FontIcon>}
+                  onClick={() => applySorting(header)}
+                  role="button"
+                  key={i}
+                  style={{ textAlign: 'center' }}
+                >
                   {specialHeaders.includes(header) ? '' : header}
                 </TableColumn>
               ))}
             </TableRow>
           </TableHeader>
           <TableBody onMouseLeave={() => onRowHover(null)}>
-            {this.getDataSlice(this.getFilteredData(searchValue)).map((row, i) => (
-              <TableRow
-                className={row.id === (selectedRow || {}).id ? 'selected-row' : ''}
-                key={i}
-                onMouseOver={debounce(() => onRowHover(row), 5)}
-                onClick={() => {
-                  if (!onRowClick) return
-                  else if ((selectedRow || {}).id !== row.id) onRowClick(row)
-                  else onRowClick(null)
-                }}
-              >
+            {this.getDataSlice(filteredData).map((row, i) => (
+              <TableRow className={row.id === (selectedRow || {}).id ? 'selected-row' : ''} key={i} onMouseOver={debounce(() => onRowHover(row), 5)}>
                 {(row => {
                   const cols = []
                   Object.keys(row)
@@ -122,7 +167,17 @@ export default class extends PureComponent {
                       // Get the header index
                       const index = headers[col]
                       cols[index] = (
-                        <TableColumn plain={false} key={j} style={{ cursor: 'pointer' }}>
+                        <TableColumn
+                          onClick={() => {
+                            if (specialHeaders.includes(col)) return
+                            else if (!onRowClick) return
+                            else if ((selectedRow || {}).id !== row.id) onRowClick(row)
+                            else onRowClick(null)
+                          }}
+                          plain={false}
+                          key={j}
+                          style={{ cursor: 'pointer' }}
+                        >
                           {row[col] === null || row[col] === undefined
                             ? '-'
                             : row[col].constructor === String
@@ -139,7 +194,7 @@ export default class extends PureComponent {
           <TablePagination
             defaultRowsPerPage={5}
             rowsPerPageItems={[5, 10, 25, 50]}
-            rows={this.getFilteredData(searchValue).length}
+            rows={filteredData.length}
             rowsPerPageLabel={'Rows'}
             onPagination={this.handlePagination}
           />
