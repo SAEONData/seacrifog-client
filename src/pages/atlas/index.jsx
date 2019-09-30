02fdba87d41668a9f4b6a31851ec77db8893d912
Zach Smith <zachsmith.sa@gmail.com>
2019-09-30 16:56:17 +0200

    started looking into clustering

--- src/pages/atlas/index.jsx
+++ src/pages/atlas/index.jsx
@@ -14,54 +14,61 @@ import Point from 'ol/geom/Point'
 import { Circle as CircleStyle, Fill, Stroke, Style, Text } from 'ol/style'
 
 class Atlas extends PureComponent {
-  state = { menuOpen: false }
-
   constructor(props) {
     super(props)
+    this.state = {
+      menuOpen: false,
+      siteSearchTerm: '',
+      sites: this.getClusteredData(this.props.sites)
+    }
 
-    this.layers = [
-      new TileLayer({
-        source: new TileWMS({
-          url: 'https://ahocevar.com/geoserver/wms',
-          params: {
-            LAYERS: 'ne:NE1_HR_LC_SR_W_DR',
-            TILED: true
-          }
-        })
-      }),
-      new VectorLayer({
-        source: new Cluster({
-          distance: 100,
-          source: new VectorSource({
-            features: this.props.sites.map(site => {
-              const xyz = JSON.parse(site.xyz).coordinates
-              return new Feature(new Point([xyz[0], xyz[1]]))
-            })
-          })
-        }),
-        style: function(feature) {
-          var size = feature.get('features').length
-          return new Style({
-            image: new CircleStyle({
-              radius: size > 200 ? 40 : size > 100 ? 30 : size > 20 ? 20 : 15,
-              stroke: new Stroke({
-                color: '#fff'
-              }),
-              fill: new Fill({
-                color: '#3399CC'
-              })
-            }),
-            text: new Text({
-              text: size.toString(),
-              fill: new Fill({
-                color: '#fff'
-              })
-            })
-          })
+    this.baseMap = new TileLayer({
+      source: new TileWMS({
+        url: 'https://ahocevar.com/geoserver/wms',
+        params: {
+          LAYERS: 'ne:NE1_HR_LC_SR_W_DR',
+          TILED: true
         }
       })
-    ]
+    })
+
+    this.clusteredSitesSource = new Cluster({
+      distance: 100,
+      source: new VectorSource({
+        features: this.state.sites
+      })
+    })
+
+    this.clusteredSites = new VectorLayer({
+      source: this.clusteredSitesSource,
+      style: function(feature) {
+        var size = feature.get('features').length
+        return new Style({
+          image: new CircleStyle({
+            radius: size > 200 ? 40 : size > 100 ? 30 : size > 20 ? 20 : 15,
+            stroke: new Stroke({
+              color: '#fff'
+            }),
+            fill: new Fill({
+              color: '#3399CC'
+            })
+          }),
+          text: new Text({
+            text: size.toString(),
+            fill: new Fill({
+              color: '#fff'
+            })
+          })
+        })
+      }
+    })
   }
+
+  getClusteredData = sites =>
+    sites.map(site => {
+      const xyz = JSON.parse(site.xyz).coordinates
+      return new Feature(new Point([xyz[0], xyz[1]]))
+    })
 
   openDrawer = () => {
     this.setState({ menuOpen: true })
@@ -75,8 +82,17 @@ class Atlas extends PureComponent {
     this.setState({ menuOpen })
   }
 
+  searchSites = val => {
+    // https://stackoverflow.com/questions/50967221/openlayers-how-to-refresh-cluster
+    const sites = this.getClusteredData([])
+    this.setState({ siteSearchTerm: val, sites }, () => {
+      this.clusteredSitesSource.source.clear()
+      this.clusteredSitesSource.getSource().addFeatures(sites)
+    })
+  }
+
   render() {
-    const { menuOpen } = this.state
+    const { menuOpen, siteSearchTerm } = this.state
     const closeBtn = (
       <Button icon onClick={this.closeDrawer}>
         close
@@ -93,43 +109,17 @@ class Atlas extends PureComponent {
           position={'right'}
           onVisibilityChange={this.handleVisibility}
           navItems={[
-            {
-              key: 'inbox',
-              primaryText: 'Inbox',
-              leftIcon: <FontIcon>inbox</FontIcon>,
-              active: true
-            },
-            {
-              key: 'starred',
-              primaryText: 'Starred',
-              leftIcon: <FontIcon>star</FontIcon>
-            },
-            {
-              key: 'send-mail',
-              primaryText: 'Send mail',
-              leftIcon: <FontIcon>send</FontIcon>
-            },
-            {
-              key: 'drafts',
-              primaryText: 'Drafts',
-              leftIcon: <FontIcon>drafts</FontIcon>
-            },
-            { key: 'divider', divider: true },
-            {
-              key: 'all-mail',
-              primaryText: 'All mail',
-              leftIcon: <FontIcon>mail</FontIcon>
-            },
-            {
-              key: 'trash',
-              primaryText: 'Trash',
-              leftIcon: <FontIcon>delete</FontIcon>
-            },
-            {
-              key: 'spam',
-              primaryText: 'Spam',
-              leftIcon: <FontIcon>info</FontIcon>
-            }
+            <TextField
+              key={'site-search'}
+              style={{ margin: '0 20px', width: '200px' }}
+              id="atlas-search-field"
+              label="Search sites"
+              placeholder="(site name)"
+              value={siteSearchTerm}
+              onChange={val => this.searchSites(val)}
+              leftIcon={<FontIcon>search</FontIcon>}
+              fullWidth={true}
+            />
           ]}
           header={<Toolbar nav={closeBtn} className="md-divider-border md-divider-border--bottom" />}
         />
@@ -138,7 +128,7 @@ class Atlas extends PureComponent {
           viewOptions={{
             zoom: 3
           }}
-          layers={this.layers}
+          layers={[this.baseMap, this.clusteredSites]}
         />
 
         <Button style={{ position: 'absolute', top: 10, right: 10 }} raised onClick={this.openDrawer}>
