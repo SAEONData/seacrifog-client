a005863fa52481e3dbdcbab777c1ee18fc285ede
Zach Smith <zachsmith.sa@gmail.com>
2019-10-24 12:21:10 +0200

    slight refactoring of the atlas, to make the index the openLayers wrapper to make it easier to think about the atlas

--- src/pages/atlas/index.jsx
+++ src/pages/atlas/index.jsx
@@ -1,10 +1,77 @@
-import React from 'react'
-import DataQuery from '../../modules/data-query'
-import { ENTIRE_GRAPH } from '../../graphql/queries'
-import Layout from './modules'
+import React, { PureComponent } from 'react'
+import { OpenLayers, clusterLayer, clusterSource, openStreetLayers, ahocevarBaseMap } from './open-layers'
+import Filter from './modules/filter'
+import Reporting from './modules/reporting'
+import Info from './modules/info'
+import Popups from './modules/popups'
+import sift from 'sift'
 
-export default () => (
-  <DataQuery query={ENTIRE_GRAPH} variables={{}}>
-    {data => <Layout data={data} />}
-  </DataQuery>
-)
+export default class extends PureComponent {
+  constructor(props) {
+    super(props)
+
+    // Get the data
+    this.sites = props.data.sites
+    this.xrefSitesNetworks = props.data.xrefSitesNetworks
+    this.networks = props.data.networks.filter(sift({ id: { $in: this.xrefSitesNetworks.map(x => x.network_id) } }))
+    this.xrefNetworksVariables = props.data.xrefNetworksVariables.filter(
+      sift({ network_id: { $in: this.networks.map(x => x.id) } })
+    )
+    this.variables = props.data.variables.filter(
+      sift({ id: { $in: this.xrefNetworksVariables.map(x => x.variable_id) } })
+    )
+    this.xrefProtocolsVariables = props.data.xrefProtocolsVariables.filter(
+      sift({ variable_id: { $in: this.variables.map(v => v.id) } })
+    )
+    this.protocols = props.data.protocols.filter(
+      sift({ id: { $in: this.xrefProtocolsVariables.map(x => x.protocol_id) } })
+    )
+
+    // Create layers
+    this.clusteredSites = clusterSource(this.sites)
+    this.clusteredSitesLayer = clusterLayer(this.clusteredSites)
+    this.ahocevarBaseMap = ahocevarBaseMap
+    this.openStreetLayers = openStreetLayers
+  }
+
+  updateMapLayer = ({ source }) => {
+    this.clusteredSitesLayer.setSource(source)
+  }
+
+  render() {
+    const { clusteredSitesLayer, updateMapLayer, ahocevarBaseMap } = this
+
+    return (
+      <OpenLayers
+        viewOptions={{
+          zoom: 3
+        }}
+        layers={[ahocevarBaseMap, clusteredSitesLayer]}
+        render={({ map }) => (
+          <>
+            {/* Side menus */}
+            <Info position={1} />
+            <Filter
+              position={2}
+              map={map}
+              updateMapLayer={updateMapLayer}
+              data={{
+                sites: this.sites,
+                xrefSitesNetworks: this.xrefSitesNetworks,
+                networks: this.networks,
+                xrefNetworksVariables: this.xrefNetworksVariables,
+                variables: this.variables,
+                xrefProtocolsVariables: this.xrefProtocolsVariables,
+                protocols: this.protocols
+              }}
+            />
+            <Reporting position={3} />
+
+            {/* No UI */}
+            <Popups map={map} />
+          </>
+        )}
+      />
+    )
+  }
+}
