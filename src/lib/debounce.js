2966b622b29c4ea4763fea5f362661cd79c62d1c
Zach Smith <zachsmith.sa@gmail.com>
2020-01-17 16:54:52 +0200

    Improved the debounce function

--- src/lib/debounce.js
+++ src/lib/debounce.js
@@ -1,8 +1,10 @@
 /**
  * Only the last function call is executed within a given time period
  */
-let timer
-export default (cb, duration = 0) => (...args) => {
-  clearTimeout(timer)
-  timer = setTimeout(() => cb(...args), duration)
+export default (cb, duration = 0) => {
+  var timer
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => cb(...args), duration)
+  }
 }
